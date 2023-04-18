891569a4b7f28520f85ccd9942f0eac1ac75ce95
Mvictor <mvictordossantos.limite@gmail.com>
2023-04-18 17:01:47 -0300

    Update ReadWeight.ino

--- examples/ReadWeight/ReadWeight.ino
+++ examples/ReadWeight/ReadWeight.ino
@@ -1,32 +1,32 @@
 /**
- * Read weight from a load cell using the ADS1230 IC in the LoadCell Nanoshield.
- *
- * This example uses the following default settings:
- *  - CS pin set to D8 (D8 jumper closed)
- *  - High gain (GAIN jumper closed)
- *  - Averaging of the latest 10 samples
- *
- * Copyright (c) 2015 Circuitar
- * This software is released under the MIT license. See the attached LICENSE file for details.
- */
+  * Leia o peso de uma célula de carga usando o IC ADS1230 no LoadCell Nanoshield.
+  *
+  * Este exemplo usa as seguintes configurações padrão:
+  * - Pino CS definido como D8 (jumper D8 fechado)
+  * - Alto ganho (jumper GAIN fechado)
+  * - Média das últimas 10 amostras
+  *
+  * Copyright (c) 2015 Circuitar
+  * Este software é lançado sob a licença MIT. Consulte o arquivo de LICENÇA em anexo para obter detalhes.
+  */
 #include <SPI.h>
 #include <Nanoshield_LoadCell.h>
 
-// Load cell with 100kg capacity and 3mV/V sensitivity
+// Célula de carga com capacidade de 100kg e sensibilidade de 3mV/V
 Nanoshield_LoadCell loadCell(100000, 3);
 
 void setup() {
-  Serial.begin(9600);
-  loadCell.begin();
+   Serial.begin(9600);
+   loadCell.begin();
   
-  // Wait for calibration and set current value to zero weight (tare)
-  while (!loadCell.updated());
-  loadCell.setZero();
+   // Aguarda a calibração e configura o valor atual para peso zero (tara)
+   while (!loadCell.updated());
+   loadCell.setZero();
 }
 
-void loop() {
-  if (loadCell.updated()) {
-    Serial.print(loadCell.getWeight(), 0);
-    Serial.println("g");
-  }
+loop void() {
+   if (loadCell.updated()) {
+     Serial.print(loadCell.getWeight(), 0);
+     Serial.println("g");
+   }
 }
